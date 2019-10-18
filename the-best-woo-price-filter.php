edb32585131af0d0b830764343d9f27f2eabac31
writ3it <42292507+writ3it@users.noreply.github.com>
2019-10-18 21:29:01 +0200

    installation

---
+++ the-best-woo-price-filter.php
@@ -0,0 +1,18 @@
+<?php
+
+/**
+ * Plugin Name:       The Best Woocommerce Price Filter
+ * Plugin URI:        https://github.com/writ3it/wordpress-the-best-woo-price-filter
+ * Description:       Woocommerce implements price filter thats cannot search product variation individually! This plugin is a solution.
+ * Version:           1.0
+ * Requires at least: 5.2
+ * Requires PHP:      7.0
+ * Author:            writ3it
+ * Author URI:        https://github.com/writ3it
+ * License:           MIT
+ * License URI:       https://github.com/writ3it/wordpress-the-best-woo-price-filter/blob/master/LICENSE
+ */
+
+define('TBWPF', __FILE__);
+
+require_once __DIR__ . '/install.php';
